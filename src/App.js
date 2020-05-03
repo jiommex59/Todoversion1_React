336410ed9dcd2d970beebb6a8d4b0a39b5361e24
jiommex59 <saviourjohnson92@gmail.com>
2020-05-03 14:33:57 +0100

    adding the entire file

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Todos from './components/Todos'
+import './App.css';
+import Header from './components/layout/Header'
+import AddTodo from './components/layout/AddTodo'
+import About from './components/pages/About'
+//import {v4 as uuid} from 'uuid'
+import axios from 'axios'
+
+export class App extends Component {
+  state = {
+    todos: []
+  }
+
+  componentDidMount(){
+    axios.get('http://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.setState({ todos: res.data}))
+  }
+
+  //toggle completed
+  markComplete = (id) =>{
+    this.setState({ todos: this.state.todos.map(todo =>{
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+}
+
+//delete Todo
+delTodo = (id) =>{
+  axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res => this.setState({ todos: [...this.state.todos.filter( todo => todo.id !== id)]}));
+}
+
+//add Todo
+addTodo = (title) =>{
+  axios.post('http://jsonplaceholder.typicode.com/todos', {
+    title,
+    completed:false
+  })
+    .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+  
+}
+
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <div className="container" >
+          <Header  />
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo} />
+              <Todos todos ={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+
+            </React.Fragment>
+          )} />
+          <Route path="/about" component={About} />
+          
+        </div>
+        
+      </div>
+      </Router>
+    )
+  }
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About Us</h1>
+            <p>This Todo app v1.0.0 created in react, part of a self development phase to learning react</p>
+        </React.Fragment>
+    )
+}
