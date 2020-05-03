f8c754c69d4283b422ebf5ed89247f69e8cd2e26
jiommex59 <saviourjohnson92@gmail.com>
2020-05-03 14:36:49 +0100

    changed header title

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
         <header style={headerStyle}>
             <h1>TodoList</h1>
             <Link style={linkStyle} to="/" >Home</Link> |  
-            <Link style={linkStyle} to="/about" >About</Link>
+            <Link style={linkStyle} to="/about" >About TodoApp</Link>
         </header>
     )
 }
